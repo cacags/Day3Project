c7dfa80e8ef394385e088762d231a045b06cc8bf
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-06 23:02:22 +0200

    new change

--- src/test/java/Hello2.java
+++ src/test/java/Hello2.java
@@ -3,5 +3,6 @@ public class Hello2 {
     public static void main(String[] args) {
         System.out.println("new big changes I did");
         //awesome is everywhere
+        //Hello world
     }
 }
